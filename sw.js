ad9e4fb0d6724d13f7bf6d1d5cf6abc80827e2a1
Ilhamono <ilhamtias75@gmail.com>
2024-10-31 18:56:11 +0700

    Update sw.js

---
+++ sw.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = 'app-cache-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/styles/main.css',
+    '/scripts/main.js',
+    '/0.png',
+    '/0.png'
+];
+
+// Instalasi Service Worker
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// Mengaktifkan Service Worker
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
+// Menghandle fetch request
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
