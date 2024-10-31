2ac6d53631b42cde38fb8908a22141b39b7aabdc
Ilhamono <ilhamtias75@gmail.com>
2024-10-31 19:01:20 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,47 +1,7 @@
-const CACHE_NAME = 'app-cache-v1';
-const urlsToCache = [
-    '/',
-    '/index.html',
-    '/styles/main.css',
-    '/scripts/main.js',
-    '/0.png',
-    '/0.png'
-];
-
-// Instalasi Service Worker
-self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => {
-                return cache.addAll(urlsToCache);
-            })
-    );
+self.addEventListener('install', function(event) {
+    console.log('Service worker installed.');
 });
 
-// Mengaktifkan Service Worker
-self.addEventListener('activate', event => {
-    event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames.map(cacheName => {
-                    if (cacheName !== CACHE_NAME) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
-        })
-    );
-});
-
-// Menghandle fetch request
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => {
-                if (response) {
-                    return response;
-                }
-                return fetch(event.request);
-            })
-    );
+self.addEventListener('activate', function(event) {
+    console.log('Service worker activated.');
 });
